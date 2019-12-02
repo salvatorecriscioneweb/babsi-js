a004a82f289d69ff5502c44bc4a2a87ff7c22a83
Salvatore Criscione <me@salvatorecriscione.com>
2019-12-02 23:10:29 +0100

    working webp interface, now missing logic

--- lib/webp.c
+++ lib/webp.c
@@ -2,6 +2,17 @@
 #include "emscripten.h"
 #include "../libwebp/src/webp/decode.h"
 
+int results[3];
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t* create_buffer(int width, int height) {
+  return malloc(width * height * 4 * sizeof(uint8_t));
+}
+
+EMSCRIPTEN_KEEPALIVE
+void destroy_buffer(uint8_t* p) {
+  free(p);
+}
 
 EMSCRIPTEN_KEEPALIVE
 int version() {
@@ -18,7 +29,8 @@ int getInfo(const uint8_t* data, size_t size) {
   // (const uint8_t* data, size_t size, int* w, int* h) -> int;
   WebPGetInfo(data, size, &width, &height);
 
-  return width;
+  results[1] = width;
+  results[2] = height;
 }
 
 EMSCRIPTEN_KEEPALIVE
@@ -26,6 +38,22 @@ uint8_t* decode(const uint8_t* data, size_t size) {
   int width;
   int height;
 
-  uint8_t* buffer = WebPDecodeRGBA(data, size, &width, &height);
-  return buffer;
+  results[0] = WebPDecodeRGBA(data, size, &width, &height);
+  results[1] = width;
+  results[2] = height;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int getResultsFirst() {
+  return results[0];
+}
+
+EMSCRIPTEN_KEEPALIVE
+int getResultsSecond() {
+  return results[1];
+}
+
+EMSCRIPTEN_KEEPALIVE
+int getResultsThird() {
+  return results[2];
 }
--- lib/webp.sh
+++ lib/webp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+emcc -O3 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
     -I libwebp \
     webp.c \
     libwebp/src/{dec,dsp,demux,enc,mux,utils}/*.c
